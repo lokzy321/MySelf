d15266d62016c5073dd24c77ac43008cb95347b8
lokzy321 <114130670+lokzy321@users.noreply.github.com>
2022-09-22 10:45:02 +0530

    Add files via upload

---
+++ MySelf/src/contents/Contact.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import Social from '../components/Social';
+class Contact extends Component {
+    render() {
+        return (
+            <div className="condiv">
+            <h1 className="subtopic">Contact Me</h1>
+            <h3>Email  :   sayedmply@gmail.com</h3>
+            <h3>Instagram   :   @lok_zy</h3>
+            <Social />
+            </div>
+            )
+        }
+    }
+    
+    export default Contact
+    
